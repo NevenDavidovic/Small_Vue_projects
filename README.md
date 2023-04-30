52f60db0b7144065cc54738e6456d9ecc4e56390
NevenDavidovic <nenoronnie@gmmail.com>
2023-04-30 09:23:22 +0200

    Added export to text option

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Small Vue Practice projects
 
 ## 1. Random Person Generator
+
 The web app that uses an API for generating data.
 Every time the gender changes the styling changes.
 
+## 2. Task Manager
 
+The web app that is used for showing how can Vue be used for tracking tasks. It has export option (Export to text);
--- Task manager/app.js
+++ Task manager/app.js
@@ -10,5 +10,16 @@ Vue.createApp({
       this.goals.push(this.enteredValue);
       this.enteredValue = "";
     },
+    exportToTxt() {
+      const data = this.goals.join("\n");
+      const blob = new Blob([data], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "tasks.txt";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    },
   },
 }).mount("#app");
